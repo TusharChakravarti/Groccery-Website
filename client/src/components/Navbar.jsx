d0174f9d21517af9fd5956e58ec51ad2ac234b9c
Tushar Chakravarti <tusharch04092005@gmail.com>
2026-02-13 17:10:43 +0530

    khaofresh

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -125,7 +125,7 @@ function Navbar() {
                     {user && (
                         <NavLink to='/my-orders' onClick={() => setOpen(false)}>My Orders</NavLink>
                     )}
-                 <NavLink to="/" onClick={() => setOpen(false)}>Contact</NavLink>
+                 <NavLink to="/contact" onClick={() => setOpen(false)}>Contact</NavLink>
 
                     {!user ? (
                         <button onClick={() =>{ setOpen(false); setShowUserLogin(true);}} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
