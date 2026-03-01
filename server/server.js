a6fb9e58724d22673c9358738af5d82f6199f41a
Tushar Chakravarti <tusharch04092005@gmail.com>
2026-03-01 10:03:36 +0530

    khaofresh

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
 // Middleware Configuration
 app.use(express.json({ limit: '50mb' })); 
-//app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cookieParser());
 app.use((req, res, next) => {
